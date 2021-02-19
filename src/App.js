ca2fa2508d9e8d90a52dce3517a60308e8c77a82
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-02-19 17:26:31 +0700

    done with form login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route path="/dashboard">
           <Home />
         </Route>
         <Route path="/scan">
           {/* <Scan /> */}
+        </Route>
+        <Route exact path='/'>
+          <Login />
         </Route>
       </Switch>
     </Router>

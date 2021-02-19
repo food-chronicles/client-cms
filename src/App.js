9cccc34463d1398bb79283d8a5fdfe250c4b55df
janakraman <hifzhana@gmail.com>
2021-02-19 13:46:54 +0700

    initial skeleton

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/scan">
+          {/* <Scan /> */}
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;

1d7af3b667dac88bfc83f1aa47b96fff7d351284
janakraman <hifzhana@gmail.com>
2021-02-19 21:39:03 +0700

    added mock QR code generator after create

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -8,7 +8,7 @@ function Create() {
       </h1>
       <h2 className="text-lg">Create new entry</h2>
       {/* <div className="flex justify-content-center "> */}
-        <div className="max-w-lg mx-auto p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+        <div className="max-w-lg mx-auto p-6 bg-gray-100 my-10 rounded-lg shadow-xl">
           <div className="ml-6 pt-1 flex-grow">
             <FormCreate />
           </div>

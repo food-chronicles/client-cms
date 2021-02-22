33aceb8e24c49ad1c4054d4385e5f79e8613bc1e
janakraman <hifzhana@gmail.com>
2021-02-22 22:01:18 +0700

    dashboard v1 done

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -5,33 +5,46 @@ const initialState = {
   category: "",
   history: [],
   isLoading: false,
-  error: null
+  isLoadingHistory: false,
+  error: null,
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_USER":
-      const { _id, username, company_name, category, history } = action.payload;
+      const { _id, username, company_name, category } = action.payload;
       return {
         ...state,
         _id,
         username,
         company_name,
         category,
-        history,
+      };
+
+    case "GET_USER_HISTORY":
+      const history = action.payload;
+      return {
+        ...state,
+        history
       };
 
     case "SET_LOADING":
       return {
         ...state,
-        isLoading: action.payload
-      }
-    
-      case "SET_ERROR":
+        isLoading: action.payload,
+      };
+
+    case "SET_LOADING_HISTORY":
       return {
         ...state,
-        error: action.payload
-      }
+        isLoadingHistory: action.payload,
+      };
+
+    case "SET_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+      };
 
     case "LOGOUT":
       return initialState;
